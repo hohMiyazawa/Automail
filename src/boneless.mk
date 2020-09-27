4d74e6cf31f3116cbe935d40d048a9b075df22e2
hoh miyazawa <hohMiyazawa@yandex.com>
2020-09-27 16:01:44 +0200

    more automation

--- src/boneless.mk
+++ src/boneless.mk
@@ -1,9 +1,23 @@
+# not yet complete!
+# is not able to build a valid version of Boneless yet!
+
 EXECUTABLES = sed rm mkdir date zip m4 cp cat
 K := $(foreach exec,$(EXECUTABLES),\
 	$(if $(shell which $(exec)),some string,$(error "Could not find the dependency '$(exec)'. If you are unable to install it, there are complete builds of Automail at https://github.com/hohMiyazawa/Automail/releases")))
 
-all: pre-build boneless_build boneless_build/build/automail.js post-build
+all: pre-build build/boneless.js
 
 pre-build:
-	rm -rf boneless_build
-	mkdir boneless_build
+	make makefile
+	rm build/boneless.js
+
+build/boneless.js:
+	cp build/automail.js build/boneless.js
+	sed -i 's/http:\/\/tampermonkey.net\//Boneless/' build/boneless.js
+	sed -i 's/2751 = automail, //' build/boneless.js
+	sed -i 's/client_id=2751/client_id=1933/' build/boneless.js
+	sed -i 's/370473-automail/411621-boneless/' build/boneless.js
+	sed -i 's/https:\/\/github\.com\/hohMiyazawa\/Automail//' build/boneless.js
+	sed -i 's/https:\/\/addons\.mozilla\.org\/en-US\/firefox\/addon\/automail\//NO KNOWN BUILDS/' build/boneless.js
+	sed -i 's/Automail/Boneless/g' build/boneless.js
+	sed -i 's/automail/boneless/g' build/boneless.js
