8449ea9741fc53ceecafc8191de2ca9cefc424f2
hoh miyazawa <hohMiyazawa@yandex.com>
2020-12-21 21:19:17 +0100

    better side header handling

--- src/modules/middleClickLinkFixer.js
+++ src/modules/middleClickLinkFixer.js
@@ -11,6 +11,26 @@ function linkFixer(){
 		recentThreads.innerText = "";
 		create("a",false,"Forum Activity",recentThreads)
 			.href = "/forum/overview";
+		let sectionHeaders = document.querySelectorAll(".section-header");
+		Array.from(sectionHeaders).forEach(header => {
+			if(header.innerText.match("Trending")){
+				header.innerText = "";
+				create("a",false,"Trending Anime",header)
+					.href = "https://anilist.co/search/anime/trending";
+				create("a","hover-manga"," & Manga",header)
+					.href = "https://anilist.co/search/manga/trending"
+			}
+			else if(header.innerText.match("Newly Added Anime")){
+				header.innerText = "";
+				create("a",false,"Newly Added Anime",header)
+					.href = "https://anilist.co/search/anime/new"
+			}
+			else if(header.innerText.match("Newly Added Manga")){
+				header.innerText = "";
+				create("a","hover-manga","Newly Added Manga",header)
+					.href = "https://anilist.co/search/manga/new"
+			}
+		})
 	}
 	else{
 		setTimeout(linkFixer,2000)//invisible change, does not take priority
