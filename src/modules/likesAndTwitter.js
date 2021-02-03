3c1659b51c7b9430e877973f5268e45e863fe756
hoh miyazawa <hohMiyazawa@yandex.com>
2021-02-03 21:42:42 +0100

    cleaned up tweet handling

--- src/modules/likesAndTwitter.js
+++ src/modules/likesAndTwitter.js
@@ -34,9 +34,9 @@ query($id: Int){
 			)
 		}
 	});
-	if(useScripts.tweets){
+	if(useScripts.tweets){//not enabled by default
 		document.querySelectorAll(
-			`.markdown a[href^="https://twitter.com/"]`
+			`.markdown a[href^="https://twitter.com/"][href*="/status/"]`
 		).forEach(tweet => {
 			if(tweet.classList.contains("hohEmbedded")){
 				return
@@ -57,20 +57,21 @@ query($id: Int){
 			let tweetBlockQuoteInnerInner = create("a","hohEmbedded","Loading tweet by " + tweetMatch[1] + "...",tweetBlockQuoteInner)
 				.href = tweet.href;
 			if(window.GM_xmlhttpRequest){
-				/*Only fetch external script if running in userscript mode*/
+				/*
+					Only fetch external script if running in userscript mode (window.GM_xmlhttpRequest is only available inside a userscript manager)
+					This is not allowed for Firefox addons, even if this setting is disabled by default.
+					Hence this check
+				*/
 				if(document.getElementById("automailTwitterEmbed")){
 					document.getElementById("automailTwitterEmbed").remove()
+					//rerun the embed script if we encounter multiple tweets. Likely not efficient
+					//https://github.com/hohMiyazawa/Automail/issues/52
 				}
 				let script = document.createElement("script");
 				script.setAttribute("src","https://platform.twitter.com/widgets.js");
 				script.setAttribute("async","");
 				script.id = "automailTwitterEmbed";
-				document.head.appendChild(script);
-			}
-			else{
-				let tweetBlockQuoteInnerInner = create("a","hohEmbedded","Loading tweet by " + tweetMatch[1] + "...",tweetBlockQuoteInner)
-					.href = tweet.href;
-				create("span",false,"Could not load Tweet. Twitter embedding is currently not available for Firefox addon builds of Automail",tweetBlockQuoteInner)
+				document.head.appendChild(script)
 			}
 		})
 	}
