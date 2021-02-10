1c2f7ae89b977d7c78e52975d2774e5e529d9ff6
hoh miyazawa <hohMiyazawa@yandex.com>
2021-02-10 03:03:16 +0100

    more consistent enabling of YT fullscreen

--- src/modules/youtubeFullscreen.js
+++ src/modules/youtubeFullscreen.js
@@ -11,7 +11,8 @@ if(useScripts.youtubeFullscreen){
 		document.querySelectorAll(".youtube iframe").forEach(video => {
 			if(!video.hasAttribute("allowfullscreen")){
 				video.setAttribute("allowfullscreen","allowfullscreen");
-				video.setAttribute("frameborder","0")
+				video.setAttribute("frameborder","0");
+				video.setAttribute("src",video.getAttribute("src").replace("autohide=1","autohide=0"))//https://github.com/hohMiyazawa/Automail/issues/53
 			}
 		})
 	},1000)
