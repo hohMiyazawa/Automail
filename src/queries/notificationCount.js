41b72d55f320f6c2e253cb6f071be2c512830848
hoh miyazawa <hohMiyazawa@yandex.com>
2021-02-19 15:06:31 +0100

    login instructions

--- src/queries/notificationCount.js
+++ src/queries/notificationCount.js
@@ -8,7 +8,8 @@ This is your notification count. The notifications of other users are private.`
 	}
 	else{
 		miscResults.innerText = 
-`Error: Not signed in with the script. Reading notifications requires AUTH permissions.
-You can sign in with the script from the settings page.`
+`Error: Not signed in with the script. Automail can't read notifications without being granted additional permissions.
+You can sign in with the script from the bottom of the settings page.`;
+		create("a",false,"https://anilist.co/settings/apps",miscResults).href = "https://anilist.co/settings/apps"
 	}
 }},
