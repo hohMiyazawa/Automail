0b17df2fb2d908e373079a20ebb828ec581d93d3
hoh miyazawa <hohMiyazawa@yandex.com>
2021-02-27 01:56:35 +0100

    more consisten colour picker insertion

--- src/utilities/colourPicker.js
+++ src/utilities/colourPicker.js
@@ -48,74 +48,80 @@ if(useScripts.colourPicker && (!useScripts.mobileFriendly)){
 		})
 	};applyColourStyles();
 	let colourPickerLocation = document.querySelector("#app > .wrap > .footer > .container");
-	if(colourPickerLocation){
-		const supportedColours = [
-			"--color-background",
-			"--color-foreground",
-			"--color-foreground-grey",
-			"--color-foreground-grey-dark",
-			"--color-foreground-blue",
-			"--color-foreground-blue-dark",
-			"--color-background-blue-dark",
-			"--color-overlay",
-			"--color-shadow",
-			"--color-shadow-dark",
-			"--color-text",
-			"--color-text-light",
-			"--color-text-lighter",
-			"--color-text-bright",
-			"--color-blue",
-			"--color-blue-dim",
-			"--color-white",
-			"--color-black",
-			"--color-red",
-			"--color-peach",
-			"--color-orange",
-			"--color-yellow",
-			"--color-green"
-		];
-		let colourChanger = function(){
-			useScripts.colourSettings[cpSelector.value] = {
-				"initial" :  (cpInitialBox.checked  ? cpInput.value : false),
-				"dark" :     (cpDarkBox.checked     ? cpInput.value : false),
-				"contrast" : (cpContrastBox.checked ? cpInput.value : false)
-			}
-			applyColourStyles();
-			useScripts.save()
-		};
-		let cpContainer = create("div","hohColourPicker",false,colourPickerLocation);
-		let cpTitle = create("h2",false,"Adjust Colours",cpContainer);
-		let cpInput = create("input",false,false,cpContainer);
-		cpInput.type = "color";
-		let cpSelector = create("select",false,false,cpContainer);
-		supportedColours.forEach(colour => {
-			let option = create("option",false,colour,cpSelector);
-			option.value = colour;
-		});
-		let cpDomain = create("p",false,false,cpContainer);
-		let cpInitialBox = createCheckbox(cpDomain);
-		create("span",false,"default",cpDomain);
-		let cpDarkBox = createCheckbox(cpDomain);
-		create("span",false,"dark",cpDomain);
-		let cpContrastBox = createCheckbox(cpDomain);
-		create("span",false,"contrast",cpDomain);
-		let cpSelectorChanger = function(){
-			if(useScripts.colourSettings[cpSelector.value]){
-				cpInitialBox.checked  = !!useScripts.colourSettings[cpSelector.value].initial;
-				cpDarkBox.checked     = !!useScripts.colourSettings[cpSelector.value].dark;
-				cpContrastBox.checked = !!useScripts.colourSettings[cpSelector.value].contrast;
-				cpInput.value = useScripts.colourSettings[cpSelector.value].initial
-			}
-			cpInitialBox.checked = false;
-			cpDarkBox.checked = false;
-			cpContrastBox.checked = false;
-		};
-		cpSelector.onchange = cpSelectorChanger;
-		cpInput.onchange = colourChanger;
-		cpInitialBox.onchange = colourChanger;
-		cpDarkBox.onchange = colourChanger;
-		cpContrastBox.onchange = colourChanger;
-		cpSelectorChanger()
-	}
+	let adder = function(){
+		if(colourPickerLocation){
+			const supportedColours = [
+				"--color-background",
+				"--color-foreground",
+				"--color-foreground-grey",
+				"--color-foreground-grey-dark",
+				"--color-foreground-blue",
+				"--color-foreground-blue-dark",
+				"--color-background-blue-dark",
+				"--color-overlay",
+				"--color-shadow",
+				"--color-shadow-dark",
+				"--color-text",
+				"--color-text-light",
+				"--color-text-lighter",
+				"--color-text-bright",
+				"--color-blue",
+				"--color-blue-dim",
+				"--color-white",
+				"--color-black",
+				"--color-red",
+				"--color-peach",
+				"--color-orange",
+				"--color-yellow",
+				"--color-green"
+			];
+			let colourChanger = function(){
+				useScripts.colourSettings[cpSelector.value] = {
+					"initial" :  (cpInitialBox.checked  ? cpInput.value : false),
+					"dark" :     (cpDarkBox.checked     ? cpInput.value : false),
+					"contrast" : (cpContrastBox.checked ? cpInput.value : false)
+				}
+				applyColourStyles();
+				useScripts.save()
+			};
+			let cpContainer = create("div","hohColourPicker",false,colourPickerLocation);
+			let cpTitle = create("h2",false,"Adjust Colours",cpContainer);
+			let cpInput = create("input",false,false,cpContainer);
+			cpInput.type = "color";
+			let cpSelector = create("select",false,false,cpContainer);
+			supportedColours.forEach(colour => {
+				let option = create("option",false,colour,cpSelector);
+				option.value = colour;
+			});
+			let cpDomain = create("p",false,false,cpContainer);
+			let cpInitialBox = createCheckbox(cpDomain);
+			create("span",false,"default",cpDomain);
+			let cpDarkBox = createCheckbox(cpDomain);
+			create("span",false,"dark",cpDomain);
+			let cpContrastBox = createCheckbox(cpDomain);
+			create("span",false,"contrast",cpDomain);
+			let cpSelectorChanger = function(){
+				if(useScripts.colourSettings[cpSelector.value]){
+					cpInitialBox.checked  = !!useScripts.colourSettings[cpSelector.value].initial;
+					cpDarkBox.checked     = !!useScripts.colourSettings[cpSelector.value].dark;
+					cpContrastBox.checked = !!useScripts.colourSettings[cpSelector.value].contrast;
+					cpInput.value = useScripts.colourSettings[cpSelector.value].initial
+				}
+				cpInitialBox.checked = false;
+				cpDarkBox.checked = false;
+				cpContrastBox.checked = false;
+			};
+			cpSelector.onchange = cpSelectorChanger;
+			cpInput.onchange = colourChanger;
+			cpInitialBox.onchange = colourChanger;
+			cpDarkBox.onchange = colourChanger;
+			cpContrastBox.onchange = colourChanger;
+			cpSelectorChanger()
+		}
+		else{
+			setTimeout(adder,1000)
+		}
+	};
+	adder();
 }
 
