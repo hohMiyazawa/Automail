09914bb2159d9c601d6bff00f3954c19136cedcc
hoh miyazawa <hohMiyazawa@yandex.com>
2021-03-03 22:39:30 +0100

    brought back studio themes

--- src/modules/addStudioBrowseSwitch.js
+++ src/modules/addStudioBrowseSwitch.js
@@ -13,40 +13,22 @@ function addStudioBrowseSwitch(){
 	}
 	let themeSwitch = create("div",["theme-switch","hohThemeSwitch"],false,target);
 	target.classList.add("cardView");
-	let compactListView = create("span",false,false,themeSwitch);
 	let listView = create("span",false,false,themeSwitch);
-	let compactView = create("span",false,false,themeSwitch);
+	listView.title = "List View";
 	let cardView = create("span","active",false,themeSwitch);
-	compactListView.appendChild(svgAssets2.listView.cloneNode(true));
+	cardView.title = "Card View";
 	listView.appendChild(svgAssets2.bigListView.cloneNode(true));
-	compactView.appendChild(svgAssets2.compactView.cloneNode(true));
-	cardView.appendChild(svgAssets2.cardView.cloneNode(true));
-	compactView.onclick = function(){
-		document.querySelector(".hohThemeSwitch .active").classList.remove("active");
-		compactView.classList.add("active");
-		target.classList.remove("cardView");
-		target.classList.remove("listView");
-		target.classList.remove("compactListView");
-	}
+	cardView.appendChild(svgAssets2.compactView.cloneNode(true));
 	cardView.onclick = function(){
 		document.querySelector(".hohThemeSwitch .active").classList.remove("active");
 		cardView.classList.add("active");
 		target.classList.add("cardView");
 		target.classList.remove("listView");
-		target.classList.remove("compactListView");
 	}
 	listView.onclick = function(){
 		document.querySelector(".hohThemeSwitch .active").classList.remove("active");
 		listView.classList.add("active");
-		target.classList.add("cardView");
+		target.classList.remove("cardView");
 		target.classList.add("listView");
-		target.classList.remove("compactListView");
-	}
-	compactListView.onclick = function(){
-		document.querySelector(".hohThemeSwitch .active").classList.remove("active");
-		compactListView.classList.add("active");
-		target.classList.add("cardView");
-		target.classList.remove("listView");
-		target.classList.add("compactListView");
 	}
 }
