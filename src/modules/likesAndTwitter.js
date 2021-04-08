699b69c5befd44cae6f13c1355a8b318a51f7bf1
hoh miyazawa <hohMiyazawa@yandex.com>
2021-04-08 19:03:34 +0200

    load more forum comment likes

--- src/modules/likesAndTwitter.js
+++ src/modules/likesAndTwitter.js
@@ -1,3 +1,4 @@
+//TODO split these?
 let likeLoop = setInterval(function(){
 	document.querySelectorAll(
 		".activity-entry > .wrap > .actions .action.likes:not(.hohHandledLike)"
@@ -44,6 +45,84 @@ query($id: Int){
 					thingy.dataset.cacheLikeCount = data.data.Activity.likes.length
 				}
 			)
+		}
+	});
+	document.querySelectorAll(
+		".forum-thread .comment .actions .like-wrap.thread_comment:not(.hohHandledLike)"
+	).forEach(thingy => {
+		thingy.classList.add("hohHandledLike");
+		let updateLikes = function(){
+			let idLink = thingy.parentNode.querySelector('.hidden[href^="/forum/thread/"]');
+			if(!idLink){
+				return
+			}
+			const id = parseInt(idLink.href.match(/\d+$/));
+//wow, this API sucks!
+			generalAPIcall(`
+query($id: Int){
+	ThreadComment(id:$id){
+		id
+		likes{
+			name
+			avatar{large}
+		}
+		childComments
+	}
+}`,
+				{id: id},
+				data => {
+					if(!data){
+						return
+					}
+					//TODO: be more efficient and update other comments too
+					let seeker = function(comment){
+						if(comment.id === id){
+							let userList = thingy.querySelector(".users");
+							let waitForAnilist = function(tries){
+								tries--;
+								if(!userList.children.length && tries){
+									setTimeout(function(){waitForAnilist(tries)},200);
+									return
+								}
+								for(let i=5;i<comment.likes.length;i++){
+									let newEle = userList.children[0].cloneNode();//to be up to date with those random attributes
+									newEle.href = "/user/" + comment.likes[i].name + "/";
+									newEle.style.backgroundImage = 'url("' + comment.likes[i].avatar.large + '")';
+									userList.appendChild(newEle)
+								}
+							};waitForAnilist(20);
+							return true
+						}
+						else if(comment.childComments){
+							for(let i=0;i<comment.childComments.length;i++){
+								if(seeker(comment.childComments[i])){
+									return true
+								}
+							}
+						}
+						return false
+					}
+					seeker(data.data.ThreadComment[0])
+				}
+			)
+		}
+		thingy.onmouseover = function(){
+			if(!thingy.querySelector(".count")){
+				return
+			}
+			let likeCount = parseInt(thingy.querySelector(".count").innerText);
+			if(likeCount <= 5){
+				return
+			}
+			if(thingy.classList.contains("hohLoadedLikes")){
+				return
+			}
+			thingy.classList.add("hohLoadedLikes");
+			updateLikes()
+		}
+		thingy.onclick = function(){
+			//TODO handle this locally
+			setTimeout(updateLikes,2000)
 		}
 	});
 	if(useScripts.tweets){//not enabled by default
