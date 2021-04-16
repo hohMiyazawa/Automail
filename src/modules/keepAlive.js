2f07ad33e8821d8fef1fa5adecfb3f4436bfcd53
hoh miyazawa <hohMiyazawa@yandex.com>
2021-04-16 21:13:43 +0200

    release as experimental

--- src/modules/keepAlive.js
+++ src/modules/keepAlive.js
@@ -1,9 +1,10 @@
 exportModule({
 	id: "keepAlive",
 	description: "Keep connections alive to prevent 'Session expired' errors [EXPERIMENTAL]",
+	extendedDescription: `Not perfectly working yet. Info: https://github.com/hohMiyazawa/Automail/issues/65`,
 	isDefault: false,
 	importance: 0,
-	categories: ["Script"],
+	categories: ["Script","Newly Added"],
 	visible: true
 })
 
@@ -14,6 +15,7 @@ new MutationObserver(function(){
 			return response.text()
 		}).then(function(html){
 			let token = html.match(/window\.al_token\ =\ "([a-zA-Z0-9]+)";/);
+			console.log("token",token);
 			if(!token){
 				return//idk, stuff changed, better do nothing
 			}
