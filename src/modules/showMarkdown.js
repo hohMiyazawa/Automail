69bd675104429303afa5f7bead292314db7bc8a7
hoh miyazawa <hohMiyazawa@yandex.com>
2021-04-22 21:31:03 +0200

    fail gracefully on private messages

--- src/modules/showMarkdown.js
+++ src/modules/showMarkdown.js
@@ -1,36 +1,40 @@
 function showMarkdown(id){
 	if(!location.pathname.match(id)){
-		return;
+		return
 	}
 	if(document.querySelector(".hohGetMarkdown")){
-		return;
+		return
 	}
 	let timeContainer = document.querySelector(".activity-text .time,.activity-message .time");
 	if(!timeContainer){
 		setTimeout(function(){showMarkdown(id)},200);
-		return;
+		return
 	};
+	if(!useScripts.accessToken && document.querySelector(".private-badge")){
+		return//can't fetch private messages without privileges
+	}
 	let codeLink = create("span",["action","hohGetMarkdown"],"</>",false,"font-weight:bolder;");
 	timeContainer.insertBefore(codeLink,timeContainer.firstChild);
 	codeLink.onclick = function(){
 		let activityMarkdown = document.querySelector(".activity-markdown");
 		if(activityMarkdown.style.display === "none"){
 			document.querySelector(".hohMarkdownSource").style.display = "none";
-			activityMarkdown.style.display = "initial";
+			activityMarkdown.style.display = "initial"
 		}
 		else{
 			activityMarkdown.style.display = "none";
 			let markdownSource = document.querySelector(".hohMarkdownSource");
 			if(markdownSource){
-				markdownSource.style.display = "initial";
+				markdownSource.style.display = "initial"
 			}
 			else{
-				generalAPIcall("query($id:Int){Activity(id:$id){...on MessageActivity{text:message}...on TextActivity{text}}}",{id:id},function(data){
+				const caller = (document.querySelector(".private-badge") ? authAPIcall : generalAPIcall);
+				caller("query($id:Int){Activity(id:$id){...on MessageActivity{text:message}...on TextActivity{text}}}",{id:id},function(data){
 					if(!location.pathname.match(id) || !data){
-						return;
+						return
 					};
 					markdownSource = create("div",["activity-markdown","hohMarkdownSource"],data.data.Activity.text,activityMarkdown.parentNode);
-				},"hohGetMarkdown" + id,20*1000);
+				},"hohGetMarkdown" + id,20*1000)
 			}
 		}
 	}
