359f7b10579d5c8f75626da258f54f2bb3e86fbd
hoh miyazawa <hohMiyazawa@yandex.com>
2021-08-08 12:30:04 +0200

    handle odd API values

--- src/modules/enhanceSocialTabFeed.js
+++ src/modules/enhanceSocialTabFeed.js
@@ -48,45 +48,51 @@ function enhanceSocialTabFeed(){
 				else{
 					loadMore.style.display = "none"
 				}
-				const randomData = "data-v-b1fca210";
 				if(data.data.Page.activities.length === 0){
 					create("div","activity-entry","No matching activities",feedContent)
 				}
 				data.data.Page.activities.forEach(act => {
-					let activityEntry = create("div",["activity-entry","activity-" + URLstuff[1] + "_list"],false,feedContent);activityEntry.setAttribute(randomData,"");
-						let wrap = create("div","wrap",false,activityEntry);wrap.setAttribute(randomData,"");
-							let list = create("div","list",false,wrap);list.setAttribute(randomData,"");
-								let cover = create("a",["cover","router-link-active"],false,list);cover.setAttribute(randomData,"");
+					let activityEntry = create("div",["activity-entry","activity-" + URLstuff[1] + "_list"],false,feedContent);
+						let wrap = create("div","wrap",false,activityEntry);
+							let list = create("div","list",false,wrap);
+								let cover = create("a",["cover","router-link-active"],false,list);
 								cover.href = "/" + URLstuff[1] + "/" + URLstuff[2] + "/" + safeURL(act.media.title.userPreferred);
 								cover.style.backgroundImage = `url("${act.media.coverImage.medium}")`;
-								let details = create("div","details",false,list);details.setAttribute(randomData,"");
-									let name = create("a","name",act.user.name,details);name.setAttribute(randomData,"");
+								let details = create("div","details",false,list);
+									let name = create("a","name",act.user.name,details);
 									name.href = "/user/" + act.user.name;
 									details.appendChild(document.createTextNode(" "));
-									let status = create("div","status",act.status + (act.progress ? " " + act.progress + " of " : " "),details);status.setAttribute(randomData,"");
-										let link = create("a",["title","router-link-active"]," " + act.media.title.userPreferred,status);link.setAttribute(randomData,"")
+									if(!act.status){//old "null" values from the API
+										if(URLstuff[1] === "manga"){
+											act.status = "read"
+										}
+										else{
+											act.status = "watched"
+										}
+									}
+									let status = create("div","status",act.status + (act.progress ? " " + act.progress + " of " : " "),details);
+										let link = create("a",["title","router-link-active"]," " + act.media.title.userPreferred,status);
 											link.href = "/" + URLstuff[1] + "/" + URLstuff[2] + "/" + safeURL(act.media.title.userPreferred);
-									let avatar = create("a","avatar",false,details);avatar.setAttribute(randomData,"");
+									let avatar = create("a","avatar",false,details);
 									avatar.href = "/user/" + act.user.name;
 									avatar.style.backgroundImage = `url("${act.user.avatar.medium}")`;
-							let timeWrapper = create("div","time",false,wrap);timeWrapper.setAttribute(randomData,"");
-								let action = create("a","action",false,timeWrapper);action.setAttribute(randomData,"");
+							let timeWrapper = create("div","time",false,wrap);
+								let action = create("a","action",false,timeWrapper);
 								action.appendChild(svgAssets2.link.cloneNode(true));
 								action.href = "/activity/" + act.id;
-								let time = nativeTimeElement(act.createdAt);timeWrapper.appendChild(time);time.setAttribute(randomData,"");
-							let actions = create("div","actions",false,wrap);actions.setAttribute(randomData,"");
-								let actionReplies = create("div",["action","replies"],false,actions);actionReplies.setAttribute(randomData,"");
+								let time = nativeTimeElement(act.createdAt);timeWrapper.appendChild(time);
+							let actions = create("div","actions",false,wrap);
+								let actionReplies = create("div",["action","replies"],false,actions);
 									if(act.replies.length){
-										let replyCount = create("span","count",act.replies.length,actionReplies);replyCount.setAttribute(randomData,"");
+										let replyCount = create("span","count",act.replies.length,actionReplies);
 										actionReplies.appendChild(document.createTextNode(" "));
 									};
 									actionReplies.appendChild(svgAssets2.reply.cloneNode(true));
 								actions.appendChild(document.createTextNode(" "));
-								let actionLikes = create("div",["action","likes"],false,actions);actionLikes.setAttribute(randomData,"");
-								const randomData2 = "data-v-977827fa";
-									let likeWrap = create("div","like-wrap",false,actionLikes);likeWrap.setAttribute(randomData,"");likeWrap.setAttribute(randomData2,"");
-										let likeButton = create("div","button",false,likeWrap);likeButton.setAttribute(randomData2,"");
-											let likeCount = create("span","count",act.likes.length || "",likeButton);likeCount.setAttribute(randomData2,"");
+								let actionLikes = create("div",["action","likes"],false,actions);
+									let likeWrap = create("div","like-wrap",false,actionLikes);
+										let likeButton = create("div","button",false,likeWrap);
+											let likeCount = create("span","count",act.likes.length || "",likeButton);
 											likeButton.appendChild(document.createTextNode(" "));
 											likeButton.appendChild(svgAssets2.likeNative.cloneNode(true));
 										likeButton.title = act.likes.map(a => a.name).join("\n");
@@ -124,7 +130,7 @@ function enhanceSocialTabFeed(){
 											};
 											likeButton.title = act.likes.map(a => a.name).join("\n")
 										};
-						let replyWrap = create("div","reply-wrap",false,activityEntry,"display:none;");replyWrap.setAttribute(randomData,"");
+						let replyWrap = create("div","reply-wrap",false,activityEntry,"display:none;");
 						actionReplies.onclick = function(){
 							if(replyWrap.style.display === "none"){
 								replyWrap.style.display = "block"
@@ -133,24 +139,22 @@ function enhanceSocialTabFeed(){
 								replyWrap.style.display = "none"
 							}
 						};
-						const randomDataReplies = "data-v-7ce9ffb8";
-						let activityReplies = create("div","activity-replies",false,replyWrap);activityReplies.setAttribute(randomData,"");activityReplies.setAttribute(randomDataReplies,"");
-						const rnd = "data-v-0664fa9f";
+						let activityReplies = create("div","activity-replies",false,replyWrap);
 						act.replies.forEach(rep => {
-							let reply = create("div","reply",false,activityReplies);reply.setAttribute(randomDataReplies,"");reply.setAttribute(rnd,"");
-								let header = create("div","header",false,reply);header.setAttribute(rnd,"");
-									let repAvatar = create("a","avatar",false,header);repAvatar.setAttribute(rnd,"");
+							let reply = create("div","reply",false,activityReplies);
+								let header = create("div","header",false,reply);
+									let repAvatar = create("a","avatar",false,header);
 									repAvatar.href = "/user/" + rep.user.name;
 									repAvatar.style.backgroundImage = `url("${rep.user.avatar.medium}")`;
 									header.appendChild(document.createTextNode(" "));
-									let repName = create("a","name",rep.user.name,header);repName.setAttribute(rnd,"");
+									let repName = create("a","name",rep.user.name,header);
 									repName.href = "/user/" + rep.user.name;
-									let cornerWrapper = create("div","actions",false,header);cornerWrapper.setAttribute(rnd,"");
-										let repActionLikes = create("div",["action","likes"],false,cornerWrapper,"display: inline-block");repActionLikes.setAttribute(rnd,"");
+									let cornerWrapper = create("div","actions",false,header);
+										let repActionLikes = create("div",["action","likes"],false,cornerWrapper,"display: inline-block");
 											const randomDataHate = "data-v-977827fa";
-											let repLikeWrap = create("div","like-wrap",false,repActionLikes);repLikeWrap.setAttribute(rnd,"");likeWrap.setAttribute(randomDataHate,"");
-												let repLikeButton = create("div","button",false,repLikeWrap);likeButton.setAttribute(randomDataHate,"");
-													let repLikeCount = create("span","count",rep.likes.length || "",repLikeButton);repLikeCount.setAttribute(randomDataHate,"");
+											let repLikeWrap = create("div","like-wrap",false,repActionLikes);
+												let repLikeButton = create("div","button",false,repLikeWrap);
+													let repLikeCount = create("span","count",rep.likes.length || "",repLikeButton);
 													repLikeButton.appendChild(document.createTextNode(" "));
 													repLikeButton.appendChild(svgAssets2.likeNative.cloneNode(true));
 												repLikeButton.title = rep.likes.map(a => a.name).join("\n");
@@ -190,10 +194,10 @@ function enhanceSocialTabFeed(){
 													};
 													repLikeButton.title = rep.likes.map(a => a.name).join("\n")
 												};
-										let repActionTime = create("div",["action","time"],false,cornerWrapper,"display: inline-block");repActionTime.setAttribute(rnd,"");
-											let repTime = nativeTimeElement(rep.createdAt);repActionTime.appendChild(repTime);repTime.setAttribute(randomData,"");
-								let replyMarkdown = create("div","reply-markdown",false,reply);replyMarkdown.setAttribute(rnd,"");
-									let markdown = create("div","markdown",false,replyMarkdown);markdown.setAttribute(rnd,"");
+										let repActionTime = create("div",["action","time"],false,cornerWrapper,"display: inline-block");
+											let repTime = nativeTimeElement(rep.createdAt);repActionTime.appendChild(repTime);
+								let replyMarkdown = create("div","reply-markdown",false,reply);
+									let markdown = create("div","markdown",false,replyMarkdown);
 									markdown.innerHTML = rep.text;//reason for inner HTML: preparsed sanitized HTML from the Anilist API
 						});
 				})
