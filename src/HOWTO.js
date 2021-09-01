cba032840ee0b1d48f50b8c07d295385d80e355f
hoh miyazawa <hohMiyazawa@yandex.com>
2021-09-01 18:31:46 +0200

    howto helptext

--- src/HOWTO.js
+++ src/HOWTO.js
@@ -6,7 +6,7 @@ exportModule({
 	id: "howto",//an unique identified for your module
 	description: "what your module does",
 	extendedDescription: `
-A long description of what your module does.
+A more detailed description of what your module does. (optional)
 
 This appears when people click the "more info" icon (🛈) on the settings page.
 	`,
@@ -14,7 +14,7 @@ This appears when people click the "more info" icon (🛈) on the settings page.
 	importance: 0,//a number, which determines the order of the settings page. Higher numbers are more important. Leave it as 0 if unsure.
 	categories: ["Script"],//what categories your module belongs in
 	//Notifications, Feeds, Forum, Lists, Profiles, Stats, Media, Navigation, Browse, Script, Login, Newly Added
-	visible: false,//if the module should be visible in the settings
+	visible: false,//if the module should be visible in the settings (REMEMBER TO CHANGE THIS TO TRUE!)
 	urlMatch: function(url,oldUrl){//a function that returns true when on the parts of the site you want it to run. url is the current url, oldUrl is the previous page
 		//example: return url === "https://anilist.co/reviews"
 		return false;
