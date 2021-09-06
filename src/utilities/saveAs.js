80d6bc89ed1be53629c015e87105028e28dd655e
hoh miyazawa <hohMiyazawa@yandex.com>
2021-09-06 15:34:47 +0200

    saveAs defaults

--- src/utilities/saveAs.js
+++ src/utilities/saveAs.js
@@ -1,13 +1,12 @@
 function saveAs(data,fileName,pureText){
-	//todo: support for browsers without blobs?
 	let link = create("a");
 	document.body.appendChild(link);
 	let json = pureText ? data : JSON.stringify(data);
 	let blob = new Blob([json],{type: "octet/stream"});
 	let url = window.URL.createObjectURL(blob);
 	link.href = url;
-	link.download = fileName || "File from Anilist.co";
+	link.download = fileName || translate("$default_filename");
 	link.click();
 	window.URL.revokeObjectURL(url);
-	document.body.removeChild(link);
+	document.body.removeChild(link)
 }
