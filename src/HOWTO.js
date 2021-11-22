7d1d16c29027e9e78ca2c198f0c8211433e2f8c4
hoh miyazawa <hohMiyazawa@yandex.com>
2021-11-22 12:57:18 +0100

    don't imply all modules should have the script category

--- src/HOWTO.js
+++ src/HOWTO.js
@@ -12,7 +12,7 @@ This appears when people click the "more info" icon (🛈) on the settings page.
 	`,
 	isDefault: false,
 	importance: 0,//a number, which determines the order of the settings page. Higher numbers are more important. Leave it as 0 if unsure.
-	categories: ["Script"],//what categories your module belongs in
+	categories: [],//what categories your module belongs in
 	//Notifications, Feeds, Forum, Lists, Profiles, Stats, Media, Navigation, Browse, Script, Login, Newly Added
 	visible: false,//if the module should be visible in the settings (REMEMBER TO CHANGE THIS TO TRUE!)
 	urlMatch: function(url,oldUrl){//a function that returns true when on the parts of the site you want it to run. url is the current url, oldUrl is the previous page
