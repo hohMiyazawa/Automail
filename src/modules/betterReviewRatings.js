445ea3e151c7e72c60c8bc047dce7d1a343df3c3
hoh miyazawa <hohMiyazawa@yandex.com>
2021-12-04 02:45:16 +0100

    drive-by localisation

--- src/modules/betterReviewRatings.js
+++ src/modules/betterReviewRatings.js
@@ -12,6 +12,26 @@ function betterReviewRatings(){
 	reviews.forEach(likeElement => {//trigger creation of the tooltips (they don't exist before hover)
 		likeElement.dispatchEvent(new Event("mouseenter"));
 		likeElement.dispatchEvent(new Event("mouseleave"));
+		//bonus: add some alias and localisation
+		let showId;
+		if(likeElement.parentNode.previousElementSibling.classList.contains("banner")){//unreliable: they load separately. But better than nothing
+			let possibleRefId = likeElement.parentNode.previousElementSibling.style.backgroundImage.match(/banner\/n?(\d+)-/);
+			if(possibleRefId){
+				showId = parseInt(possibleRefId[1])
+			}
+		}
+		if(useScripts.partialLocalisationLanguage !== "English" || aliases.has(showId)){
+			let elements = likeElement.previousElementSibling.previousElementSibling.textContent.match(/Review of (.+) by (.+)$/);
+			if(elements){
+				likeElement.previousElementSibling.previousElementSibling.childNodes[0].textContent
+					= translate(
+						"$review_reviewTitle",[
+							titlePicker({id: showId, title: {romaji: elements[1]}}),
+							elements[2]
+						]
+					)
+			}
+		}
 	});
 	setTimeout(function(){//give anilist some time to generate them
 		reviews.forEach(likeElement => {
