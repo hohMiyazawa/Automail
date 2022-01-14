491758a107dbdfc4cae371ef68735b943aa92fe2
hoh miyazawa <hohMiyazawa@yandex.com>
2022-01-14 21:32:38 +0100

    polyfill Array.prototype.flat

--- src/polyfills.js
+++ src/polyfills.js
@@ -98,3 +98,38 @@ if(!window.BroadcastChannel){
 
     window.BroadcastChannel = window.BroadcastChannel || BroadcastChannel;
 }
+
+/**
+ * Array.flat() polyfill
+ * Adapted from https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/flat#reduce_concat_isArray_recursivity
+ */
+if (!Array.prototype.flat) {
+	Array.prototype.flat = function(depth) {
+
+		'use strict';
+
+		// If no depth is specified, default to 1
+		if (depth === undefined) {
+			depth = 1;
+		}
+
+		// Recursively reduce sub-arrays to the specified depth
+		var flatten = function (arr, depth) {
+
+			// If depth is 0, return the array as-is
+			if (depth < 1) {
+				return arr.slice();
+			}
+
+			// Otherwise, concatenate into the parent array
+			return arr.reduce(function (acc, val) {
+				return acc.concat(Array.isArray(val) ? flatten(val, depth - 1) : val);
+			}, []);
+
+		};
+
+		return flatten(this, depth);
+
+	};
+}
+
