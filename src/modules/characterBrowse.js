d078a9c67532289c36b858249c3c4ec5305000e5
Zarin <kazzarin@protonmail.com>
2022-03-16 01:51:41 -0400

    characterBrowse: use mutationobserver instead of settimeout

--- src/modules/characterBrowse.js
+++ src/modules/characterBrowse.js
@@ -4,57 +4,56 @@ exportModule({
 	isDefault: true,
 	categories: ["Browse"],
 	visible: false,
-	urlMatch: function(url,oldUrl){
-		return url.match(/^https:\/\/anilist\.co\/search\/characters/)
+	urlMatch: function(url){
+		return /^https:\/\/anilist\.co\/search\/characters\/?(favorites)?$/.test(url)
 	},
-	code: async function(){
-		if(
-			!document.URL.match(/\/search\/characters\/?(favorites)?$/)
-		){
-			return
-		}
+	code: function(){
+		let pageCount = 0;
+		let perPage = 30;
 		const query = `
-query($page: Int!){
-	Page(page: $page,perPage: 20){
+query($page: Int!,$perPage: Int!){
+	Page(page: $page,perPage: $perPage){
 		characters(sort: [FAVOURITES_DESC]){
 			id
 			favourites
 		}
 	}
 }`;
-		let favCallback = async function(data,page){
-			if(!document.URL.match(/\/search\/characters\/?(favorites)?$/)){
-				return
-			}
-			let resultsToTag = document.querySelectorAll(".results.cover .staff-card,.landing-section.characters .staff-card");
-			if(resultsToTag.length < page*data.Page.characters.length){
-				setTimeout(function(){
-					favCallback(data,page)
-				},200);//may take some time to load
-				return;
-			}
-			data = data.Page.characters;
-			data.forEach((character,index) => create(
+		const results = document.querySelector(".landing-section.characters > .results, .results.cover");
+		let charCount = results.childElementCount;
+
+		const insertFavs = function(data){
+			const chars = data.Page.characters;
+			chars.forEach((character,index) => create(
 				"span",
 				"hohFavCountBrowse",
 				character.favourites,
-				resultsToTag[(page - 1)*data.length + index]
+				results.children[(pageCount - 1)*chars.length + index]
 			).title = translate("$characterBrowseTooltip"));
-			const {ndata, errors} = await anilistAPI(query, {
-				variables: {page: page + 1}
+		}
+
+		const getFavs = async function(){
+			pageCount++
+			const {data, errors} = await anilistAPI(query, {
+				variables: {page: pageCount, perPage}
 			})
 			if(errors){
 				return;
 			}
-			return favCallback(ndata, page + 1);
-		};
-		const {data, errors} = await anilistAPI(query, {
-			variables: {page: 1}
-		})
-		if(errors){
-			return;
+			return insertFavs(data);
 		}
-		return favCallback(data, 1);
+
+		if(!/\/search\/characters\/?$/.test(location.pathname)){ // full favorites page
+			perPage = 20;
+			new MutationObserver((_mutations) => {
+				if(results.childElementCount !== charCount && results.childElementCount % 20 === 0){
+					charCount = results.childElementCount;
+					getFavs();
+				}
+			}).observe(results, { subtree: true, childList: true })
+		}
+
+		getFavs();
 	},
 	css: `
 .hohFavCountBrowse{
