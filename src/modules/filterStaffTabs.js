766f07ac750932591c00c6fbbadc246a0fae568d
Zarin <kazzarin@protonmail.com>
2022-05-25 03:02:55 -0400

    filterStaffTabs: use watchElem instead of loop

--- src/modules/filterStaffTabs.js
+++ src/modules/filterStaffTabs.js
@@ -7,84 +7,69 @@ exportModule({
 	urlMatch: function(url,oldUrl){
 		return url.match(/^https:\/\/anilist\.co\/(anime|manga)\/\d+\/.*\/staff/)
 	},
-	code: function(){
-		let waiter = function(tries){
-			if(!document.URL.match(/^https:\/\/anilist\.co\/(anime|manga)\/\d+\/.*\/staff/)){
-				return
-			}
-			if(tries > 10){
-				return
-			}
-			let mediaStaff = document.querySelector(".media-staff");
-			if(!mediaStaff){
-				setTimeout(function(){waiter(0)},250);
-				return
-			}
-			let staffGrid = mediaStaff.querySelector(".grid-wrap");
-			if(staffGrid.children.length > 9){
-				let filterBoxContainer = create("div","#hohStaffTabFilter");
-				mediaStaff.prepend(filterBoxContainer);
-				let filterRemover = create("span","#hohFilterRemover",svgAssets.cross,filterBoxContainer)
-				let filterBox = create("input",false,false,filterBoxContainer);
-				filterBox.placeholder = "Filter by name or role";
-				filterBox.setAttribute("list","staffRoles");
-				let filterer = function(){
-					let val = filterBox.value;
-					Array.from(staffGrid.children).forEach(card => {
-						if(
-							looseMatcher(card.querySelector(".name").innerText,val)
-							|| looseMatcher(card.querySelector(".role").innerText,val)
-						){
-							card.style.display = "inline-grid"
-						}
-						else{
-							card.style.display = "none"
-						}
-					});
-					if(val === ""){
-						filterRemover.style.display = "none"
+	code: async function(){
+		const mediaStaff = document.querySelector(".media-staff") || await watchElem(".media-staff");
+		const staffGrid = mediaStaff.querySelector(".grid-wrap") || await watchElem(".grid-wrap",mediaStaff);
+		if(staffGrid.children.length > 9){
+			let filterBoxContainer = create("div","#hohStaffTabFilter");
+			mediaStaff.prepend(filterBoxContainer);
+			let filterRemover = create("span","#hohFilterRemover",svgAssets.cross,filterBoxContainer)
+			let filterBox = create("input",false,false,filterBoxContainer);
+			filterBox.placeholder = "Filter by name or role";
+			filterBox.setAttribute("list","staffRoles");
+			let filterer = function(){
+				let val = filterBox.value;
+				Array.from(staffGrid.children).forEach(card => {
+					if(
+						looseMatcher(card.querySelector(".name").innerText,val)
+						|| looseMatcher(card.querySelector(".role").innerText,val)
+					){
+						card.style.display = "inline-grid"
 					}
 					else{
-						filterRemover.style.display = "inline"
+						card.style.display = "none"
 					}
-				}
-				filterRemover.onclick = function(){
-					filterBox.value = "";
-					filterer()
-				}
-				filterBox.oninput = filterer;
-				let dataList = create("datalist","#staffRoles",false,filterBoxContainer);
-				let buildStaffRoles = function(){
-					let autocomplete = new Set();
-					Array.from(staffGrid.children).forEach(card => {
-						autocomplete.add(card.querySelector(".name").innerText);
-						autocomplete.add(card.querySelector(".role").innerText.replace(/\s*\(.*\)\.?\s*/,""));
-						if(card.querySelector(".role").innerText.includes("OP")){
-							autocomplete.add("OP")
-						}
-						if(card.querySelector(".role").innerText.includes("ED")){
-							autocomplete.add("ED")
-						}
-					})
-					removeChildren(dataList);
-					autocomplete.forEach(
-						value => create("option",false,false,dataList).value = value
-					)
-				};buildStaffRoles();
-				let mutationConfig = {
-					attributes: false,
-					childList: true,
-					subtree: false
-				};
-				let observer = new MutationObserver(function(){
-					filterer();
-					buildStaffRoles()
 				});
-				observer.observe(staffGrid,mutationConfig)
+				if(val === ""){
+					filterRemover.style.display = "none"
+				}
+				else{
+					filterRemover.style.display = "inline"
+				}
 			}
-			else{
-				setTimeout(function(){waiter(++tries)},250 + tries*100)
+			filterRemover.onclick = function(){
+				filterBox.value = "";
+				filterer()
 			}
-		};waiter(0)
+			filterBox.oninput = filterer;
+			let dataList = create("datalist","#staffRoles",false,filterBoxContainer);
+			let buildStaffRoles = function(){
+				let autocomplete = new Set();
+				Array.from(staffGrid.children).forEach(card => {
+					autocomplete.add(card.querySelector(".name").innerText);
+					autocomplete.add(card.querySelector(".role").innerText.replace(/\s*\(.*\)\.?\s*/,""));
+					if(card.querySelector(".role").innerText.includes("OP")){
+						autocomplete.add("OP")
+					}
+					if(card.querySelector(".role").innerText.includes("ED")){
+						autocomplete.add("ED")
+					}
+				})
+				removeChildren(dataList);
+				autocomplete.forEach(
+					value => create("option",false,false,dataList).value = value
+				)
+			};buildStaffRoles();
+			let mutationConfig = {
+				attributes: false,
+				childList: true,
+				subtree: false
+			};
+			let observer = new MutationObserver(function(){
+				filterer();
+				buildStaffRoles()
+			});
+			observer.observe(staffGrid,mutationConfig)
+		}
 	}
 })
