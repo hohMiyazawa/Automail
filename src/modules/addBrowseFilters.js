403206e5e49011b418bc883b016866141653a535
Zarin <kazzarin@protonmail.com>
2023-02-20 06:01:17 -0500

    assign custom sorts directly

--- src/modules/addBrowseFilters.js
+++ src/modules/addBrowseFilters.js
@@ -11,40 +11,14 @@ function addBrowseFilters(type){
 		}
 		sorts.classList.add("hohAlready")
 	}
-
-	let applySorts = function(){
-		if(! /^\/search/.test(location.pathname)){
-			return
-		}
-		let dropdown = sorts.querySelector(".dropdown");
-		if(!dropdown){
-			setTimeout(applySorts,200);
-			return
-		}
-		let alreadyAdded = document.querySelectorAll(".hohSorts");
-		alreadyAdded.forEach(aready => aready.remove());
-		const linkHandler = function(elem, sort){
-			elem.onclick = () => {
-				sorts.__vue__.setSort(sort);
-				sorts.__vue__.close();
-			}
-		};
-		if(type === "anime"){
-			let episodeSort = create("div",["option","hohSorts"],"Episodes ↓",dropdown);
-			let episodeSortb = create("div",["option","hohSorts"],"Episodes ↑",dropdown);
-			linkHandler(episodeSort, "EPISODES_DESC")
-			linkHandler(episodeSortb, "EPISODES")
-		}
-		else if(type === "manga"){
-			let chapterSort = create("div",["option","hohSorts"],"Chapters ↓",dropdown);
-			let chapterSortb = create("div",["option","hohSorts"],"Chapters ↑",dropdown);
-			let volumeSort = create("div",["option","hohSorts"],"Volumes ↓",dropdown);
-			let volumeSortb = create("div",["option","hohSorts"],"Volumes ↑",dropdown);
-			linkHandler(chapterSort, "CHAPTERS_DESC")
-			linkHandler(chapterSortb, "CHAPTERS")
-			linkHandler(volumeSort, "VOLUMES_DESC")
-			linkHandler(volumeSortb, "VOLUMES")
-		}
+	const customAnime = {EPISODES_DESC: "Episodes ↓", EPISODES: "Episodes ↑"};
+	const customManga = {CHAPTERS_DESC: "Chapters ↓", CHAPTERS: "Chapters ↑", VOLUMES_DESC: "Volumes ↓", VOLUMES: "Volumes ↑"};
+	if(type === "anime"){
+		Object.keys(customManga).forEach(key => delete sorts.__vue__.sortOptions[key])
+		Object.assign(sorts.__vue__.sortOptions, customAnime)
 	}
-	sorts.addEventListener("click", applySorts)
+	else if(type === "manga"){
+		Object.keys(customAnime).forEach(key => delete sorts.__vue__.sortOptions[key])
+		Object.assign(sorts.__vue__.sortOptions, customManga)
+	}
 }
