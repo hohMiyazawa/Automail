5b881e3d39991b6c0d8578d5b26cb82426d9e26a
Zarin <kazzarin@protonmail.com>
2023-02-20 21:29:11 -0500

    dropdown order is determined by insertion order

--- src/modules/addBrowseFilters.js
+++ src/modules/addBrowseFilters.js
@@ -12,21 +12,26 @@ function addBrowseFilters(type){
 		sorts.classList.add("hohAlready")
 	}
 	const customSorts = {
-		TITLE_ROMAJI: "Title ↑", TITLE_ROMAJI_DESC: "Title ↓",
-		POPULARITY_DESC: "Popularity ↓", POPULARITY: "Popularity ↑",
-		SCORE_DESC: "Average Score ↓", SCORE: "Average Score ↑",
-		FAVOURITES_DESC: "Favorites ↓", FAVOURITES: "Favorites ↑",
-		ID_DESC: "Date Added ↓", ID: "Date Added ↑",
-		START_DATE_DESC: "Release Date ↓", START_DATE: "Release Date ↑"
+		TITLE_ROMAJI: "Title ↑",
+		TITLE_ROMAJI_DESC: "Title ↓",
+		POPULARITY_DESC: "Popularity ↓",
+		POPULARITY: "Popularity ↑",
+		SCORE_DESC: "Average Score ↓",
+		SCORE: "Average Score ↑",
+		TRENDING_DESC: "Trending",
+		FAVOURITES_DESC: "Favorites",
+		ID_DESC: "Date Added",
+		START_DATE_DESC: "Release Date ↓",
+		START_DATE: "Release Date ↑"
 	};
 	const customAnime = {EPISODES_DESC: "Episodes ↓", EPISODES: "Episodes ↑", DURATION_DESC: "Duration ↓", DURATION: "Duration ↑"};
 	const customManga = {CHAPTERS_DESC: "Chapters ↓", CHAPTERS: "Chapters ↑", VOLUMES_DESC: "Volumes ↓", VOLUMES: "Volumes ↑"};
 	if(type === "anime"){
-		Object.keys(customManga).forEach(key => delete sorts.__vue__.sortOptions[key])
+		Object.keys(sorts.__vue__.sortOptions).forEach(key => delete sorts.__vue__.sortOptions[key])
 		Object.assign(sorts.__vue__.sortOptions, customSorts, customAnime)
 	}
 	else if(type === "manga"){
-		Object.keys(customAnime).forEach(key => delete sorts.__vue__.sortOptions[key])
+		Object.keys(sorts.__vue__.sortOptions).forEach(key => delete sorts.__vue__.sortOptions[key])
 		Object.assign(sorts.__vue__.sortOptions, customSorts, customManga)
 	}
 }
