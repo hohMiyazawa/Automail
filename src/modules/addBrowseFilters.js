ce49153aea3b95d38e802b389fa639107c8d715b
Zarin <kazzarin@protonmail.com>
2023-02-20 20:10:18 -0500

    add additional sort options

--- src/modules/addBrowseFilters.js
+++ src/modules/addBrowseFilters.js
@@ -11,14 +11,22 @@ function addBrowseFilters(type){
 		}
 		sorts.classList.add("hohAlready")
 	}
-	const customAnime = {EPISODES_DESC: "Episodes ↓", EPISODES: "Episodes ↑"};
+	const customSorts = {
+		TITLE_ROMAJI: "Title ↑", TITLE_ROMAJI_DESC: "Title ↓",
+		POPULARITY_DESC: "Popularity ↓", POPULARITY: "Popularity ↑",
+		SCORE_DESC: "Average Score ↓", SCORE: "Average Score ↑",
+		FAVOURITES_DESC: "Favorites ↓", FAVOURITES: "Favorites ↑",
+		ID_DESC: "Date Added ↓", ID: "Date Added ↑",
+		START_DATE_DESC: "Release Date ↓", START_DATE: "Release Date ↑"
+	};
+	const customAnime = {EPISODES_DESC: "Episodes ↓", EPISODES: "Episodes ↑", DURATION_DESC: "Duration ↓", DURATION: "Duration ↑"};
 	const customManga = {CHAPTERS_DESC: "Chapters ↓", CHAPTERS: "Chapters ↑", VOLUMES_DESC: "Volumes ↓", VOLUMES: "Volumes ↑"};
 	if(type === "anime"){
 		Object.keys(customManga).forEach(key => delete sorts.__vue__.sortOptions[key])
-		Object.assign(sorts.__vue__.sortOptions, customAnime)
+		Object.assign(sorts.__vue__.sortOptions, customSorts, customAnime)
 	}
 	else if(type === "manga"){
 		Object.keys(customAnime).forEach(key => delete sorts.__vue__.sortOptions[key])
-		Object.assign(sorts.__vue__.sortOptions, customManga)
+		Object.assign(sorts.__vue__.sortOptions, customSorts, customManga)
 	}
 }
