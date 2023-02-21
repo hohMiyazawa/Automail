3ff99c97853abb0028de1e751363884f55267470
Zarin <kazzarin@protonmail.com>
2023-02-21 02:22:52 -0500

    condense filters

--- src/queries/relatedManga.js
+++ src/queries/relatedManga.js
@@ -52,54 +52,26 @@ query($name: String!){
 			(b,a) => a.host - b.host
 		);
 		miscResults.innerText = "";
-		let foundCount = create("p",false,"Found " + found.length + " manga:",miscResults);
+		const filterSettings = [];
+		const filterData = [
+			{name: "Prequel", checked: true},
+			{name: "Sequel", checked: true},
+			{name: "Side Story", checked: true},
+			{name: "Alternative", checked: true},
+			{name: "Other Relation", checked: true},
+			{name: "Include media related to dropped manga", checked: false},
+			{name: "Include manga in related media", checked: true},
+			{name: "Include one shots in related media", checked: true},
+			{name: "Include light novels in related media", checked: true}
+		];
 		let filters = create("div",false,false,miscResults);
-
-		let row1 = create("p",false,false,filters);
-		let checkBox1 = createCheckbox(row1);
-		let label1 = create("span",false,"Prequel",row1);
-
-		let row2 = create("p",false,false,filters);
-		let checkBox2 = createCheckbox(row2);
-		let label2 = create("span",false,"Sequel",row2);
-
-		let row3 = create("p",false,false,filters);
-		let checkBox3 = createCheckbox(row3);
-		let label3 = create("span",false,"Side Story",row3);
-
-		let row4 = create("p",false,false,filters);
-		let checkBox4 = createCheckbox(row4);
-		let label4 = create("span",false,"Alternative",row4);
-
-		let row5 = create("p",false,false,filters);
-		let checkBox5 = createCheckbox(row5);
-		let label5 = create("span",false,"Other Relation",row5);
-
-		let row6 = create("p",false,false,filters);
-		let checkBox6 = createCheckbox(row6);
-		let label6 = create("span",false,"Include media related to dropped manga",row6);
-
-		let row7 = create("p",false,false,filters);
-		let checkBox7 = createCheckbox(row7);
-		let label7 = create("span",false,"Include manga in related media",row7);
-
-		let row8 = create("p",false,false,filters);
-		let checkBox8 = createCheckbox(row8);
-		let label8 = create("span",false,"Include one shots in related media",row8);
-
-		let row9 = create("p",false,false,filters);
-		let checkBox9 = createCheckbox(row9);
-		let label9 = create("span",false,"Include light novels in related media",row9);
-
-		checkBox1.checked = true;
-		checkBox2.checked = true;
-		checkBox3.checked = true;
-		checkBox4.checked = true;
-		checkBox5.checked = true;
-		checkBox7.checked = true;
-		checkBox8.checked = true;
-		checkBox9.checked = true;
-
+		filterData.forEach((filter, i) => {
+			const row = create("p",false,false,filters);
+			createCheckbox(row,"filter-"+i,filter.checked);
+			create("span",false,filter.name,row);
+			filterSettings[i] = filter.checked;
+		})
+		let foundCount = create("p",false,"Found " + found.length + " manga:",miscResults);
 		let f_results = create("div",false,false,miscResults);
 		let render = function(){
 			removeChildren(f_results);
@@ -107,16 +79,16 @@ query($name: String!){
 			found.forEach(item => {
 				if(
 					(
-						(checkBox1.checked && item.relationType.includes("PREQUEL"))
-						|| (checkBox2.checked && item.relationType.includes("SEQUEL"))
-						|| (checkBox3.checked && item.relationType.includes("SIDE_STORY"))
-						|| (checkBox4.checked && item.relationType.includes("ALTERNATIVE"))
-						|| (checkBox5.checked && item.relationType.some(type => ["ADAPTATION","CHARACTER","SUMMARY","SPIN_OFF","OTHER","SOURCE","COMPILATION","CONTAINS"].includes(type)))
+						(filterSettings[0] && item.relationType.includes("PREQUEL"))
+						|| (filterSettings[1] && item.relationType.includes("SEQUEL"))
+						|| (filterSettings[2] && item.relationType.includes("SIDE_STORY"))
+						|| (filterSettings[3] && item.relationType.includes("ALTERNATIVE"))
+						|| (filterSettings[4] && item.relationType.some(type => ["ADAPTATION","CHARACTER","SUMMARY","SPIN_OFF","OTHER","SOURCE","COMPILATION","CONTAINS"].includes(type)))
 					)
-					&& (checkBox6.checked || item.isDropped.some(val => !val))
-					&& (checkBox7.checked || item.node.format !== "MANGA")
-					&& (checkBox8.checked || item.node.format !== "ONE_SHOT")
-					&& (checkBox9.checked || item.node.format !== "NOVEL")
+					&& (filterSettings[5] || item.isDropped.some(val => !val))
+					&& (filterSettings[6] || item.node.format !== "MANGA")
+					&& (filterSettings[7] || item.node.format !== "ONE_SHOT")
+					&& (filterSettings[8] || item.node.format !== "NOVEL")
 				){
 					create("a",["link","newTab"],item.node.title.romaji,f_results,"display:block;padding:5px;")
 						.href = "/manga/" + item.node.id;
@@ -125,15 +97,12 @@ query($name: String!){
 			});
 			foundCount.innerText = "Found " + count + " manga:";
 		};
-		checkBox1.onchange = render;
-		checkBox2.onchange = render;
-		checkBox3.onchange = render;
-		checkBox4.onchange = render;
-		checkBox5.onchange = render;
-		checkBox6.onchange = render;
-		checkBox7.onchange = render;
-		checkBox8.onchange = render;
-		checkBox9.onchange = render;
+		filters.querySelectorAll(".hohCheckbox input").forEach(checkBox => {
+			checkBox.addEventListener("change",(e) => {
+				filterSettings[parseInt(e.target.id.split("-")[1])] = e.target.checked;
+				render()
+			})
+		})
 		render();
 	}
 	const data = await anilistAPI(relationQuery, {
