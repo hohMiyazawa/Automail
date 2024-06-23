7f6553c986435b7e04bddd49aadd3cf108b44b50
hohMiyazawa <sbrendberg@yandex.com>
2024-06-23 20:53:39 +0200

    silence unfinished warning

--- src/utilities.js
+++ src/utilities.js
@@ -925,7 +925,7 @@ function compatCheck(list,name,type,callback){
 //used by the stats module, and to safeguard the manga chapter guesses
 //publishing manga is a bit tricky, since Anilist doesn't track chapters
 const commonUnfinishedManga = m4_include(data/commonUnfinishedManga.json)
-if(NOW() - new Date(2022,11,11) > 365*24*60*60*1000){
+if(NOW() - new Date(2024,5,22) > 365*24*60*60*1000){
 	console.log("remind hoh to update the commonUnfinishedManga list")
 }
 
